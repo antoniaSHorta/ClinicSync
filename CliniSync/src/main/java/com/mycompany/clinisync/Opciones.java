14633529e1069e9c0764c90a47363c20d8625ac5
antoniaSHorta <142348629+antoniaSHorta@users.noreply.github.com>
2023-10-24 00:02:49 -0300

    Add files via upload
    Entrega 1 Final.

---
+++ CliniSync/src/main/java/com/mycompany/clinisync/Opciones.java
@@ -0,0 +1,27 @@
+package com.mycompany.clinisync;
+
+public class Opciones {
+
+    public void Menu() {
+
+        System.out.println("INGRESE LA OPCION");
+        System.out.println("1. Crear Datos");
+        System.out.println("2. Actualizar Datos");
+        System.out.println("3. Leer Datos");
+        System.out.println("4. Eliminar Datos");
+        System.out.println("0. Salir sistema");
+    }
+
+    public void Menu1(){
+        System.out.println("1. Leer pacientes");
+        System.out.println("2. Leer Doctores");
+        System.out.println("3. Leer Planificados");
+        System.out.println("4. Leer Historial");
+    }
+    public void Menu2(){
+        System.out.println("1. Mostrar pacientes");
+        System.out.println("2. Mostrar Doctores");
+        System.out.println("3. Mostrar Planificados");
+        System.out.println("4. Mostrar Historial");
+    }
+}
